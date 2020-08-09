f2311705017e04a05b5d811be8ce67601acd26ad
Artur <atur94@gmail.com>
2020-08-09 15:58:43 +0200

    inicjalizacja

---
+++ src/main/java/com/arbstudio/easygamesroomserver/EasygamesRoomServerApplication.java
@@ -0,0 +1,13 @@
+package com.arbstudio.easygamesroomserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasygamesRoomServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasygamesRoomServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arbstudio/easygamesroomserver/EasygamesRoomServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbstudio.easygamesroomserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasygamesRoomServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
